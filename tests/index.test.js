7a5c7cf2ffd28282874476cdf8abe205be881ad5
IzumiSy <beetle-noise@gmx.com>
2020-05-10 22:36:39 +0900

    Fix tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -21,24 +21,37 @@ describe('useRouter', () => {
   })
 
   test('transition', () => {
-    const { result } = renderHook(() =>
-      useRouter({
-        '/': () => (
-          <div data-testid="text">Here is root</div>
-        ),
-        '/aaa': () => (
-          <div data-testid="text">Here is aaa</div>
-        )
-      })
-    )
+    const runRender = () => {
+      const { result } = renderHook(() =>
+        useRouter({
+          '/': () => (
+            <div data-testid="text">Here is root</div>
+          ),
+          '/aaa': () => (
+            <div data-testid="text">Here is aaa</div>
+          )
+        })
+      )
 
-    const [history, router] = result.current
-    render(<>{router}</>)
+      return result
+    }
+
+    const result1 = runRender()
+    const [history1, _] = result1.current
 
     act(() => {
-      history.push('/aaa')
+      history1.push('/aaa')
     })
 
+    // Usually history.push triggers re-rendering by browsers.
+    // I am not sure about why, but jest does not emulate history API
+    // so here manually tries to do the same thing like re-rendering
+    // just by calling runRender() again.
+
+    const result2 = runRender()
+    const [history2, router] = result2.current
+
+    render(<>{router}</>)
     expect(screen.getByTestId('text')).toHaveTextContent('Here is aaa')
   })
 })
